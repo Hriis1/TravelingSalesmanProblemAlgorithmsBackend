9e40dc3ac4e75d8e3cf45847b059adad1432f977
hrishd <hrishd@abv.bg>
2026-04-17 21:47:55 +0300

    vars for MMAS

--- core/TSPMMAS.h
+++ core/TSPMMAS.h
@@ -30,9 +30,25 @@ private:
 
 public:
 
-    TSPMMAS(const std::vector<std::vector<int>>& adjMat, unsigned int seed = std::random_device{}()) 
-        :TSPAlgo(adjMat, seed)
+    TSPMMAS(const std::vector<std::vector<int>>& adjMat, int numAnts, int numIterations, double alpha, double beta, double rho, double tauMin, double tauMax, unsigned int seed = std::random_device{}()) 
+        :TSPAlgo(adjMat, seed), _numAnts(numAnts), _numIterations(numIterations), _alpha(alpha), _beta(beta), _rho(rho), _tauMin(tauMin), _tauMax(tauMax)
     {}
 
 private:
+    int _numAnts;        // number of ants per iteration
+    int _numIterations;  // total iterations
+
+    double _alpha;       // pheromone importance
+    double _beta;        // heuristic importance
+    double _rho;         // evaporation rate (0–1)
+
+    //Pheromone bounds
+    double _tauMin;
+    double _tauMax;
+
+    int _globalBestFreq = 10;
+
+    std::vector<Ant> _ants;
+    std::vector<std::vector<double>> _pheromone;
+    std::vector<std::vector<double>> _heuristic;
 };
