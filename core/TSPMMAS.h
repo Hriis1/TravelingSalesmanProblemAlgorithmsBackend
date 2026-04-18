617ab5ac9bd5950ec4a4f6a78b5dd91bfe87b038
hrishd <hrishd@abv.bg>
2026-04-18 16:34:35 +0300

    move matrices init to solve in mmas

--- core/TSPMMAS.h
+++ core/TSPMMAS.h
@@ -30,34 +30,29 @@ private:
 
 public:
 
-    TSPMMAS(const std::vector<std::vector<int>>& adjMat, int numAnts, int numIterations, float alpha, float beta, float rho, float tauMin, float tauMax, unsigned int seed = std::random_device{}()) 
-        :TSPAlgo(adjMat, seed), _numAnts(numAnts), _numIterations(numIterations), _alpha(alpha), _beta(beta), _rho(rho), _tauMin(tauMin), _tauMax(tauMax)
+    TSPMMAS(int numAnts, int numIterations, float alpha, float beta, float rho, float tauMin, float tauMax, unsigned int seed = std::random_device{}()) 
+        :TSPAlgo(seed), _numAnts(numAnts), _ants(numAnts), _numIterations(numIterations), _alpha(alpha), _beta(beta), _rho(rho), _tauMin(tauMin), _tauMax(tauMax)
+    {}
+
+    void solve(const std::vector<std::vector<int>>& adjMat) override
     {
         int nCities = adjMat.size();
 
         //Init pheromone matrix with _tauMax
-        _pheromone.resize(nCities, std::vector<float>(nCities, _tauMax));
+        _pheromone.assign(nCities, std::vector<float>(nCities, _tauMax));
 
         //Init the heuristic matrix with 1/<dist between cities>
-        _heuristic.resize(nCities, std::vector<float>(nCities));
+        _heuristic.assign(nCities, std::vector<float>(nCities));
         for (size_t y = 0; y < nCities; y++)
         {
             for (size_t x = 0; x < nCities; x++)
             {
-                if (y != x && (*_adjMat)[y][x] > 0)
-                    _heuristic[y][x] = 1.0f / (*_adjMat)[y][x];
+                if (y != x && adjMat[y][x] > 0)
+                    _heuristic[y][x] = 1.0f / adjMat[y][x];
                 else
                     _heuristic[y][x] = 0;
             }
         }
-
-        //Init ants
-        _ants.resize(_numAnts);
-    }
-
-    void solve() override
-    {
-
     }
 
 private:
