6c182559776a547f91af48e95f1297e15a2e5316
hrishd <hrishd@abv.bg>
2026-05-02 20:02:50 +0300

    transformed costs correctness

--- core/TSPLKH.h
+++ core/TSPLKH.h
@@ -10,13 +10,17 @@
 
 struct LKHConfig
 {
-    //Default values
-    int maxTrials = 50;
-    int maxCandidates = 15;
-    int maxDepth = 3;
-    int backtrackingLimit = 20;
-    int runs = 1;
-    int kickStrength = 4;
+    // ASIGNED VALUES ARE JUST DEFAULTS
+
+    int maxTrials = 50;                 //How many attempts LKH makes
+    int maxCandidates = 15;             //How many candidate edges each city considers
+    int maxDepth = 3;                   //Maximum depth of the variable k-opt search
+    int backtrackingLimit = 20;         //Limits how many failed alternatives are explored.
+    int runs = 1;                       //How many independent full runs to do
+    int kickStrength = 4;               //How strong the perturbation is when stuck
+
+
+    long long precision = 100;                //precision for calculating transformed costs
 };
 
 
@@ -32,10 +36,24 @@ public:
 
     void solve(const std::vector<std::vector<int>>& adjMat) override
     {
+        //num cities
+        int n = adjMat.size();
+
+        //asign penalties to 0 at the start
+        pi.assign(n, 0);
     }
 
 private:
 
+    //Returns the transformed cost between 2 cities
+    long long getTransformedCost(int i, int j, const std::vector<std::vector<int>>& adjMat) const
+    {
+        return (long long)(_config.precision * (long long)adjMat[i][j] + pi[i] + pi[j]);
+    }
+
 private:
-    LKHConfig _config;
+    LKHConfig _config; //config data for solver
+
+    std::vector<long long> pi; //pnalties
+
 };
