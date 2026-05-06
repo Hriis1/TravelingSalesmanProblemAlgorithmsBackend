c77568011703dbc39be187d241194e0aed175d9b
hrishd <hrishd@abv.bg>
2026-05-06 21:48:30 +0300

    Seperated solve func + basic helpers into a cpp file for LKH

--- core/TSPLKH.h
+++ core/TSPLKH.h
@@ -44,45 +44,15 @@ public:
 
     TSPLKH(const LKHConfig& config, unsigned int seed = std::random_device{}());
 
-    void solve(const std::vector<std::vector<int>>& adjMat) override
-    {
-        //num cities
-        int n = adjMat.size();
+    void solve(const std::vector<std::vector<int>>& adjMat) override;
 
-        //Reset values
-        _bestPisSum = 0;
-        _bestLowerBound = LLONG_MIN;
-        _bestNorm = LLONG_MAX;
-
-        //init the _nodes and reset _piSum
-        _nodes.resize(n);
-        _bestPis.assign(n, 0);
-        for (int i = 0; i < n; i++)
-        {
-            _nodes[i].id = i;
-            _nodes[i].pi = 0;
-            _nodes[i].degree = 2;
-            _nodes[i].lastDegree = 2;
-            _nodes[i].parent = -1;
-        }
-        _piSum = 0;
-
-        //Ascent and get the lower bound
-        long long lowerBound = ascent(adjMat);
-    }
 
 private:
 
     //Returns the transformed cost between 2 cities
-    long long getTransformedCost(int i, int j, const std::vector<std::vector<int>>& adjMat) const
-    {
-        return (long long)(_config.precision * (long long)adjMat[i][j] + _nodes[i].pi + _nodes[j].pi);
-    }
+    long long getTransformedCost(int i, int j, const std::vector<std::vector<int>>& adjMat) const;
 
-    long long calculateOneTreeLowerBound(long long oneTreeCost) const
-    {
-        return oneTreeCost - (2 * _piSum);
-    }
+    long long calculateOneTreeLowerBound(long long oneTreeCost) const;
 
     void updatePenalty(LKHNode& node, long long delta)
     {
