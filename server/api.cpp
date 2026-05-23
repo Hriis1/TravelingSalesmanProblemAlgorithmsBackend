cd485c8165169524546be65f682b5445ebfe5ef0
hrishd <hrishd@abv.bg>
2026-05-23 23:17:27 +0300

    use a json lib to make building a json easy

--- server/api.cpp
+++ server/api.cpp
@@ -1,13 +1,38 @@
-#include "httplib.h"
 #include <iostream>
+#include <utility>
+#include <string>
+#include <vector>
+
+#include "httplib.h"
+#include "json.hpp"
+
+std::string buildJson(const std::vector<std::pair<std::string, nlohmann::json>>& data)
+{
+    nlohmann::json j;
+
+    for (const auto& param : data)
+        j[param.first] = param.second;
+
+    return j.dump();
+}
+
+void solveTSP(const httplib::Request& req, httplib::Response& res)
+{
+    auto body = buildJson({
+        {"success", true},
+        {"message", "API works"},
+        {"distance", 10466}
+    });
+
+    res.status = 200;
+    res.set_content(body, "application/json");
+}
 
 int main()
 {
     httplib::Server server;
 
-    server.Get("/test", [](const httplib::Request& req, httplib::Response& res) {
-        res.set_content("{\"message\":\"API works\"}", "application/json");
-        });
+    server.Get("/solveTSP", solveTSP);
 
     std::cout << "Server running on http://localhost:8080\n";
     server.listen("0.0.0.0", 8080);
