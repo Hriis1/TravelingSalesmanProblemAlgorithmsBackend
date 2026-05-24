5ad411def49c534a7464b0edbcfa8d215e83624f
hrishd <hrishd@abv.bg>
2026-05-24 14:10:57 +0300

    solveTSP request func start

--- server/api.cpp
+++ server/api.cpp
@@ -27,7 +27,7 @@ std::string buildJson(std::initializer_list<std::pair<std::string, nlohmann::jso
     return buildJsonFromPairs(data);
 }
 
-void sendResponse(int status, const std::string& body, httplib::Response& res)
+void sendResponse(httplib::Response& res, int status, const std::string& body)
 {
     res.status = status;
     res.set_content(body, "application/json");
@@ -35,14 +35,42 @@ void sendResponse(int status, const std::string& body, httplib::Response& res)
 
 void solveTSP(const httplib::Request& req, httplib::Response& res)
 {
-    auto body = buildJson({
-        {"success", true},
-        {"message", "API works"},
-        {"distance", 10466}
-    });
+    nlohmann::json bodyJson;
 
-    res.status = 200;
-    res.set_content(body, "application/json");
+    try {
+        bodyJson = nlohmann::json::parse(req.body);
+    }
+    catch (...) { //Body is invalid json
+        sendResponse(res, 400,
+            buildJson({ {"success", false}, {"error", "Invalid JSON body"} }));
+        return;
+    }
+
+    //Invalid TSP instance param
+    if (!bodyJson.contains("instance") || !bodyJson["instance"].is_string()) {
+        sendResponse(res, 400,
+            buildJson({ {"success", false}, {"error", "Missing or invalid field: instance"} }));
+        return;
+    }
+
+    //Invalid Algorithm param 
+    if (!bodyJson.contains("algorithm") || !bodyJson["algorithm"].is_string()) {
+        sendResponse(res, 400,
+            buildJson({ {"success", false}, {"error", "Missing or invalid field: algorithm"} }));
+        return;
+    }
+
+    //Bulld final response
+    std::string instance = bodyJson["instance"];
+    std::string algorithm = bodyJson["algorithm"];
+    auto response = buildJson({
+        {"success", true},
+        {"instance", instance},
+        {"algorithm", algorithm}
+        });
+
+    //Send final response
+    sendResponse(res, 200, response);
 }
 
 int main()
